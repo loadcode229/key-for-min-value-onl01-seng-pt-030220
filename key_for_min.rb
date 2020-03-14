9aec69afda6d862bb4ff62f8e323b2e6d3729827
Adam Priest <sepone229@gmail.com>
2020-03-14 01:00:09 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,8 +4,6 @@
 def key_for_min_value(name_hash)
   min_value = nil
   min_key = nil
-  if name_hash == 0
-    return nil
   name_hash.each do |key, value|
     if min_value == nil || value < min_value
       min_value = value
