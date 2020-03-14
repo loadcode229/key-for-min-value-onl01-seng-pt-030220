52337b0baecb8bf1ca140645a4c2dbc0b59a4872
Adam Priest <sepone229@gmail.com>
2020-03-14 00:55:37 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  min_value = nil
+  min _key = nil
   if name_hash == 0
     return nil
-  elsif
-  
+  elsif 
   end 
 end
