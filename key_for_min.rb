5edd8cfb3cbffa264f87b4285a34aa6ce40ab607
Adam Priest <sepone229@gmail.com>
2020-03-14 00:44:53 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.collect do |name, age|
-    if age < age
-      return age
-    end
-  end
+  name_hash.collect do |age|
+    if age == :age < age
+      age
+  end 
 end
